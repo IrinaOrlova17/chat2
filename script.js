9e89b4bf80945e5b734f310b863cc3b6b9d3a33a
IrinaOrlova17 <93528037+IrinaOrlova17@users.noreply.github.com>
2022-05-01 17:31:21 +0300

    sd
    sdf

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function sutitZinu()
     console.log('sutitZinu() darbojas');
    let zina = document.querySelector('.manaZina').value;
    let zinas = document.querySelector('.chataZinas');
-   zinas.innerHTML = zina.value;
+   
+   zinas.innerHTML = zinas.innerHTML + zina.value;
 }
