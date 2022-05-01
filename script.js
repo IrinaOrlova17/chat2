c470b6df6348633246601f814321c4e133f19c5c
IrinaOrlova17 <93528037+IrinaOrlova17@users.noreply.github.com>
2022-05-01 17:20:10 +0300

    zina
    zinz

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
+   let zina = document.querySelector('.manaZina').value;
+   let zinas = document.querySelector('.chataZinas');
+   zinas.innerHTML = zina.value;
 }
