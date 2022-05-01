560b13b98b6a37f921671aff56957bb8c5b2a4aa
IrinaOrlova17 <93528037+IrinaOrlova17@users.noreply.github.com>
2022-05-01 17:42:36 +0300

    scr1
    1

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//console.log(2)
+let zina = document.querySelector('.manaZina').value;
+let zinas = document.querySelector('.chataZinas');
+
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
-   let zina = document.querySelector('.manaZina').value;
+   let zina = document.querySelector('.manaZina');
    let zinas = document.querySelector('.chataZinas');
    
-   zinas.innerHTML = zinas.innerHTML + zina.value;
+   zinas.innerHTML = zinas.innerHTML + '<br />' +zina.value;
 }
