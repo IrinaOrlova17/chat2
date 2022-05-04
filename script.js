836605c4287b5b24b81fd18a3cb6beccbd69380f
IrinaOrlova17 <93528037+IrinaOrlova17@users.noreply.github.com>
2022-05-04 10:06:56 +0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const API = "https://Chat2022.irinaorlova3.repl.co"
+
 let zina = document.querySelector('.manaZina').value;
 let zinas = document.querySelector('.chataZinas');
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
-   let zina = document.querySelector('.manaZina');
-   let zinas = document.querySelector('.chataZinas');
-   
-   zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+    fetch(API + '/sutit/Juris/' + zina.value)
 }
 async function ieladetChataZinas()
 {
